3c21d8d7e4bb16573390a5ec231ff7a25cbcefe3
GABRIEL RIBEIRO BENEVENUTO <gabrielrjrj018@gmail.com>
2025-05-26 22:33:38 -0300

    extras

---
+++ Tabuada/Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+
+
+Console.WriteLine("escreva o numero que deseja saber a tabuada");
+n = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{n} * {i} == {n * i}" );
+
+}
